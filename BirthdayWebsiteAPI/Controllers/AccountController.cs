f20559d13b5aff222f0d20581ff9d87b133eeb3b
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2026-04-06 17:41:24 -0300

    Create login routes

---
+++ BirthdayWebsiteAPI/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using BirthdayWebsiteAPI.Interface;
+using BirthdayWebsiteAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayWebsiteAPI.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                // Call the service to register the user
+                await _accountService.RegisterUser(model);
+                return Ok(new { message = "User registered successfully" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var loginInfo = await _accountService.Login(model);
+                return Ok(loginInfo);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                await _accountService.Logout();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+    }
+}
---
+++ BirthdayWebsiteAPI/Interface/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using BirthdayWebsiteAPI.ViewModels;
+
+namespace BirthdayWebsiteAPI.Interface
+{
+    public interface IAccountService
+    {
+        Task<bool> RegisterUser(RegisterViewModel model);
+        Task<LoginInfo> Login(LoginViewModel model);
+        Task Logout();
+    }
+}
