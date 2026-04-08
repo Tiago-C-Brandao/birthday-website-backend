ad75de00957e170956b37f4f1031d731b2ab1faa
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2026-04-08 16:50:03 -0300

    Create guest routes

---
+++ BirthdayWebsiteAPI/Controllers/GuestController.cs
@@ -0,0 +1,102 @@
+﻿using BirthdayWebsiteAPI.Interface;
+using BirthdayWebsiteAPI.Models;
+using BirthdayWebsiteAPI.ViewModels.Guest;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayWebsiteAPI.Controllers
+{
+    [Route("guest")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GuestController : Controller
+    {
+        private readonly IGuestService _guestService;
+
+        public GuestController(IGuestService guestService)
+        {
+            _guestService = guestService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<IEnumerable<Guest>>> GetAllGuests()
+        {
+            try
+            {
+                var guests = await _guestService.GetAllGuests();
+                return Ok(guests);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Guest>> GetGuest(int id)
+        {
+            try
+            {
+                var guest = await _guestService.GetGuest(id);
+                return Ok(guest);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateGuest([FromBody] CreateGuestViewModel model)
+        {
+            try 
+            { 
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var guest = await _guestService.CreateGuest(model);
+
+                return Ok(guest);
+
+            } catch (Exception ex) 
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPatch("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateGuest(int id, [FromBody] UpdateGuestViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                var guest = await _guestService.UpdateGuest(id, model);
+                return Ok(guest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteGuest(int id)
+        {
+            try
+            {
+                await _guestService.DeleteGuest(id);
+                return Ok(new { message = "Guest deleted successfully" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ BirthdayWebsiteAPI/Interface/IGuestService.cs
@@ -0,0 +1,16 @@
+﻿using BirthdayWebsiteAPI.Models;
+using BirthdayWebsiteAPI.ViewModels.Guest;
+
+namespace BirthdayWebsiteAPI.Interface
+{
+    public interface IGuestService
+    {
+        Task<IEnumerable<Guest>> GetAllGuests();
+        Task<Guest> GetGuest(int id);
+        Task<Guest> CreateGuest(CreateGuestViewModel model);
+        Task<Guest> UpdateGuest(int id, UpdateGuestViewModel model);
+        Task<Guest> GetGuestByWhatsApp(string whasapp);
+        Task DeleteGuest(int id);
+
+    }
+}
